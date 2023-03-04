4f5bf11b46a5abe7bb741f384afc6d8e4868c91e
Владимир Ширина <64553688+Tionthe1@users.noreply.github.com>
2023-03-04 08:53:10 +0200

    react post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Post from "./components/post/Post";
+
+const ANAKIN_IMAGE =
+  "https://i.pinimg.com/564x/cb/63/e3/cb63e3ac0d0df0a21cbedc582702846e.jpg";
+
+const RAY_IMAGE =
+  "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+const postsData = [
+  {
+    author_name: "Anakin Skywalker",
+    photo: ANAKIN_IMAGE,
+    nickname: "@dart_vader",
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: RAY_IMAGE,
+    date: "26 февр.",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post
+        nickname={postsData[0].nickname}
+        date={postsData[0].date}
+        author_name={postsData[0].author_name}
+        content={postsData[0].content}
+        photo={postsData[0].photo}
+        image={postsData[0].image}
+      />
     </div>
   );
 }
